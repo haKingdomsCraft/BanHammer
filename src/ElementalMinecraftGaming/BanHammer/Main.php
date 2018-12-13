31f20ce254fdb5f20a1a3a9664391f9dfd8e2a7a
MDC <mathewpsutherland@gmail.com>
2018-12-13 11:55:22 +0000

    Update Main.php

--- src/ElementalMinecraftGaming/BanHammer/Main.php
+++ src/ElementalMinecraftGaming/BanHammer/Main.php
@@ -1,1 +1,58 @@
-//ToDo
+<?php
+namespace ElementalMinecraftGaming\BanHammer;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\server;
+use pocketmine\Player;
+use pocketmine\command\defaults\BanIpCommand;
+use pocketmine\event\entity\EntityDamageEvent;
+class Main extends PluginBase implements listener {
+    private $hasBh = [];
+    public function onEnable() {
+        $this->getLogger()->info("Ban Hammer has been enabled!");
+    }
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool { 
+        if ($sender->hasPermission("player.bh")) {
+            if (strtolower($command->getName()) == "banhammer") {
+                if ($sender instanceof Player) {
+                    $sender->sendMessage(TextFormat::BLUE . "Ban hammer activated!") ;
+                    $this->hasPc[$sender->getName()] = true;
+                    return true;
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Incorrect potato!");
+                    return false;
+                }
+            }
+        }
+        if ($sender->hasPermission("player.bhd")) {
+            if (strtolower($command->getName()) == "disablebanhammer") {
+                if ($sender instanceof Player) {
+                    $sender->sendMessage(TextFormat::GOLD . "Deactivated Ban Hammer");
+                    if (isset($this->hasPc[$event->getPlayer()->getName()])) {
+                        unset($this->hasPc[$event->getPlayer()->getName()]);
+                    }
+                    return true;
+                } else {
+                    $sender->sendMessage(TextFormat::RED . "Incorrect potato!");
+                    return false;
+                }
+            }
+        }
+    }
+    public function onQuit(PlayerQuitEvent $event) {
+        if (isset($this->hasBh[$event->getPlayer()->getName()])) {
+            unset($this->hasBh[$event->getPlayer()->getName()]);
+        }
+    }
+    public function onEntityDamage(EntityDamageEvent $event) {
+        if (isset($this->hasBh[$event->getPlayer()->getName()])) {
+            $target = $event->getEntity() ;
+            $this->processIPBan($entity->getAddress(), $player);
+            $server->getIPBans()->addBan($ip, $reason, null);
+        }
+    }
+}
+
